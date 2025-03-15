fd14889c07f4f1b2d3e798f1500ff1dee29fe58e
Enzo Leiva <enzoleiva654@gmail.com>
2025-03-15 14:13:03 -0400

    Add files via upload

--- Desafio/ObjetoDesafio.java
+++ Desafio/ObjetoDesafio.java
@@ -1,66 +1,79 @@
 package JavaIntermediario.Desafio;
-
 import java.util.Scanner;
-
 public class ObjetoDesafio {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        Uchiha user = new Uchiha();
-        System.out.println("-------------------Monte seu Uchiha!!!-------------------");
+        Uchiha[] uchihas = new Uchiha[2];
+        Uchiha novoUchiha = new Uchiha();
 
-        System.out.print("Digite o nome do seu Uchiha: ");
-        user.nome = scan.nextLine();
-        System.out.println("Você criou um nome.");
-        System.out.println();
-
-        System.out.print("Qual a sua missão? ");
-        user.missao = scan.nextLine();
-        System.out.println("Você o deu uma missão.");
-        System.out.println();
-
-        System.out.print("Quantos anos esse Uchiha tem? ");
-        user.idade = scan.nextInt();
-        System.out.println("Você o deu uma idade.");
-        scan.nextLine();
-        System.out.println();
-
-        System.out.print("Qual o nível de dificuldade dessa missão? ");
-        user.NivelDeDificuldade = scan.nextLine();
-        System.out.println("Você o deu um nível de dificuldade.");
-        System.out.println();
-
-        System.out.print("Qual o status da missão? ");
-        user.StatusDaMissao = scan.nextLine();
-        System.out.println("Você definiu o status da missão.");
-
+        int contador = 0;
         int menu = 0;
-        while (menu != 3){
+        while (menu != 4){
             System.out.println("\n-------------Menu de opções ninjas-------------");
-            System.out.println("1 - Exibir informações do uchiha criado ");
-            System.out.println("2 - Mudar habilidade especial do Uchiha ");
-            System.out.println("3 - encerrar programa. ");
+            System.out.println("1 - Criar um Uchiha ");
+            System.out.println("2 - Atribuir a habilidade especial Uchiha ");
+            System.out.println("3 - Exibir informações dos Uchihas ");
+            System.out.println("4 - encerrar programa. ");
 
-            System.out.println("Escolha uma das 3 opções. ");
+            System.out.print("Escolha uma das 4 opções. ");
             menu = scan.nextInt();
             scan.nextLine();
 
             switch (menu){
                 case 1:
-                    user.MostrarHabilidadeEspecial();
+                    if (contador < uchihas.length){
+                        System.out.println("\n-------------------Monte seu Uchiha!!!-------------------");
+                        System.out.print("Digite o nome do seu Uchiha: ");
+                        novoUchiha.nome = scan.nextLine();
+                        System.out.println("Você criou um nome.");
+                        System.out.println();
+
+                        System.out.print("Qual a sua missão? ");
+                        novoUchiha.missao = scan.nextLine();
+                        System.out.println("Você o deu uma missão.");
+                        System.out.println();
+
+                        System.out.print("Quantos anos esse Uchiha tem? ");
+                        novoUchiha.idade = scan.nextInt();
+                        System.out.println("Você o deu uma idade.");
+                        scan.nextLine();
+                        System.out.println();
+
+                        System.out.print("Qual o nível de dificuldade dessa missão? ");
+                        novoUchiha.NivelDeDificuldade = scan.nextLine();
+                        System.out.println("Você o deu um nível de dificuldade.");
+                        System.out.println();
+
+                        System.out.print("Qual o status da missão? ");
+                        novoUchiha.StatusDaMissao = scan.nextLine();
+                        System.out.println("Você definiu o status da missão.");
+
+                        uchihas[contador] = novoUchiha;
+                        contador++;
+                    } else {
+                        System.out.println("Máximo de Uchihas cadastrados. ");
+                    }
                     break;
                 case 2:
-                    System.out.println("Qual será a nova habilidade especial desse Uchiha? ");
-                    user.habilidadeEspecial = scan.nextLine();
+                    System.out.println("Qual será a habilidade especial desse Uchiha? ");
+                    novoUchiha.habilidadeEspecial = scan.nextLine();
                     System.out.println("Habilidade atribuida.");
                     break;
                 case 3:
+                    for (int i = 0; i < uchihas.length; i++) {
+                        if (uchihas[i] != null){
+                            System.out.println(uchihas[i]);
+                            uchihas[i].MostrarHabilidadeEspecial();
+                        }
+                    }
+                    break;
+                case 4:
                     System.out.println("Encerrando programa...");
                     break;
                 default:
                     System.out.println("Opção inválida. Tente novamente. ");
             }
         }
-
     }
 }
